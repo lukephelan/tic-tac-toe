39c25e4b5b6ebaf2926d091b651e85623e354068
lukephelan <luke.phelan@hotmail.com>
2016-05-09 12:51:26 +1000

    Allows for duplicate clicks and a draw

--- js/app.js
+++ js/app.js
@@ -29,51 +29,60 @@ $('.square').click(function(){
             if (SQ1 === 0) {
                 $(this).append('<h3>' + playerOne + '</h3>');
                 SQ1 = 1;
+                player = playerTwo;
             };
         } else if (square === "SQ2") {
             if (SQ2 === 0) {
                 $(this).append('<h3>' + playerOne + '</h3>');
                 SQ2 = 1;
+                player = playerTwo;
             };
         } else if (square === "SQ3") {
             if (SQ3 === 0) {
                 $(this).append('<h3>' + playerOne + '</h3>');
                 SQ3 = 1;
+                player = playerTwo;
             };
         } else if (square === "SQ4") {
             if (SQ4 === 0) {
                 $(this).append('<h3>' + playerOne + '</h3>');
                 SQ4 = 1;
+                player = playerTwo;
             };
         } else if (square === "SQ5") {
             if (SQ5 === 0) {
                 $(this).append('<h3>' + playerOne + '</h3>');
                 SQ5 = 1;
+                player = playerTwo;
             };
         } else if (square === "SQ6") {
             if (SQ6 === 0) {
                 $(this).append('<h3>' + playerOne + '</h3>');
                 SQ6 = 1;
+                player = playerTwo;
             };
         } else if (square === "SQ7") {
             if (SQ7 === 0) {
                 $(this).append('<h3>' + playerOne + '</h3>');
                 SQ7 = 1;
+                player = playerTwo;
             };
         } else if (square === "SQ8") {
             if (SQ8 === 0) {
                 $(this).append('<h3>' + playerOne + '</h3>');
                 SQ8 = 1;
+                player = playerTwo;
             };
         } else if (square === "SQ9") {
             if (SQ9 === 0) {
                 $(this).append('<h3>' + playerOne + '</h3>');
                 SQ9 = 1;
+                player = playerTwo;
             };
         };
 
         checkWinner();
-        player = playerTwo;
+
     } else if (player === playerTwo) {
         var square = $(this).map(function(){ return this.id }).get().join(', ');
 
@@ -83,51 +92,60 @@ $('.square').click(function(){
             if (SQ1 === 0) {
                 $(this).append('<h3>' + playerTwo + '</h3>');
                 SQ1 = -1;
+                player = playerOne;
             };
         } else if (square === "SQ2") {
             if (SQ2 === 0) {
                 $(this).append('<h3>' + playerTwo + '</h3>');
                 SQ2 = -1;
+                player = playerOne;
             };
         } else if (square === "SQ3") {
             if (SQ3 === 0) {
                 $(this).append('<h3>' + playerTwo + '</h3>');
                 SQ3 = -1;
+                player = playerOne;
             };
         } else if (square === "SQ4") {
             if (SQ4 === 0) {
                 $(this).append('<h3>' + playerTwo + '</h3>');
                 SQ4 = -1;
+                player = playerOne;
             };
         } else if (square === "SQ5") {
             if (SQ5 === 0) {
                 $(this).append('<h3>' + playerTwo + '</h3>');
                 SQ5 = -1;
+                player = playerOne;
             };
         } else if (square === "SQ6") {
             if (SQ6 === 0) {
                 $(this).append('<h3>' + playerTwo + '</h3>');
                 SQ6 = -1;
+                player = playerOne;
             };
         } else if (square === "SQ7") {
             if (SQ7 === 0) {
                 $(this).append('<h3>' + playerTwo + '</h3>');
                 SQ7 = -1;
+                player = playerOne;
             };
         } else if (square === "SQ8") {
             if (SQ8 === 0) {
                 $(this).append('<h3>' + playerTwo + '</h3>');
                 SQ8 = -1;
+                player = playerOne;
             };
         } else if (square === "SQ9") {
             if (SQ9 === 0) {
                 $(this).append('<h3>' + playerTwo + '</h3>');
                 SQ9 = -1;
+                player = playerOne;
             };
         };
 
         checkWinner();
-        player = playerOne;
+
     };
 });
 
@@ -165,5 +183,7 @@ function checkWinner() {
         console.log("Player One wins!");
     } else if (SQ3 + SQ5 + SQ7 === -3) {
         console.log("Player One wins!");
+    } else if (SQ1 != 0 && SQ2 != 0 && SQ3 != 0 && SQ4 != 0 && SQ5 != 0 && SQ6 != 0 && SQ7 != 0 && SQ8 != 0 && SQ9 != 0) {
+        console.log("Game is a draw");
     };
 };
