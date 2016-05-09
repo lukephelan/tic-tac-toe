62b2dab17e7c2accc96ffd73f383628b476ac467
lukephelan <luke.phelan@hotmail.com>
2016-05-09 11:33:59 +1000

    X or O to come from an array

--- js/app.js
+++ js/app.js
@@ -1,6 +1,17 @@
-// When you click the square, add the text "X".
+// Flick between players
+
+var icon = ['X', 'O'];
+
+var playerOne = icon[0];
+var playerTwo = icon[1];
+
+var iconText = ('.square');
+
+
+// When you click the square, add the text.
 
 $('.square').click(function(){
-    $(this).append('<h3>X</h3>');
+    $(this).append('<h3>' + playerOne + '</h3>');
+    // $(this).append('<h3>X</h3>');
     console.log("CLICKED");
 });
