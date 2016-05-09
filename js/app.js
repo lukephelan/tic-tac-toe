c0d97a2209f4183b1623b9202b55d754ab190639
lukephelan <luke.phelan@hotmail.com>
2016-05-09 12:15:26 +1000

    Calculates winner

--- js/app.js
+++ js/app.js
@@ -24,17 +24,63 @@ var SQ9 = 0;
 $('.square').click(function(){
     if (player === playerOne) {
         $(this).append('<h3>' + playerOne + '</h3>');
+
+        var square = $(this).map(function(){ return this.id }).get().join(', ');
+        console.log(square);
+        if (square === "SQ1") {
+            SQ1 = 1;
+        } else if (square === "SQ2") {
+            SQ2 = 1;
+        } else if (square === "SQ3") {
+            SQ3 = 1;
+        } else if (square === "SQ4") {
+            SQ4 = 1;
+        } else if (square === "SQ5") {
+            SQ5 = 1;
+        } else if (square === "SQ6") {
+            SQ6 = 1;
+        } else if (square === "SQ7") {
+            SQ7 = 1;
+        } else if (square === "SQ8") {
+            SQ8 = 1;
+        } else if (square === "SQ9") {
+            SQ9 = 1;
+        };
+
         checkWinner();
         player = playerTwo;
     } else if (player === playerTwo) {
         $(this).append('<h3>' + playerTwo + '</h3>');
+
+        var square = $(this).map(function(){ return this.id }).get().join(', ');
+        console.log(square);
+        if (square === "SQ1") {
+            SQ1 = -1;
+        } else if (square === "SQ2") {
+            SQ2 = -1;
+        } else if (square === "SQ3") {
+            SQ3 = -1;
+        } else if (square === "SQ4") {
+            SQ4 = -1;
+        } else if (square === "SQ5") {
+            SQ5 = -1;
+        } else if (square === "SQ6") {
+            SQ6 = -1;
+        } else if (square === "SQ7") {
+            SQ7 = -1;
+        } else if (square === "SQ8") {
+            SQ8 = -1;
+        } else if (square === "SQ9") {
+            SQ9 = -1;
+        };
+
         checkWinner();
         player = playerOne;
     };
 });
 
-// Check if player has three in line
-function checkWinner () {
+// Check if player has three in-line
+function checkWinner() {
     if (SQ1 + SQ2 + SQ3 === 3) {
         console.log("Player One wins!");
     } else if (SQ1 + SQ2 + SQ3 === -3) {
@@ -59,5 +105,13 @@ function checkWinner () {
         console.log("Player One wins!");
     } else if (SQ3 + SQ6 + SQ9 === -3) {
         console.log("Player Two wins!");
+    } else if (SQ1 + SQ5 + SQ9 === 3) {
+        console.log("Player One wins!");
+    } else if (SQ1 + SQ5 + SQ9 === -3) {
+        console.log("Player One wins!");
+    } else if (SQ3 + SQ5 + SQ7 === 3) {
+        console.log("Player One wins!");
+    } else if (SQ3 + SQ5 + SQ7 === -3) {
+        console.log("Player One wins!");
     };
 };
