29e516b40bd17b16eb9bffb07d1481e179fa788c
lukephelan <luke.phelan@hotmail.com>
2016-05-10 13:21:57 +1000

    Copied easy mode as hard mode, still same as easy

--- js/app.js
+++ js/app.js
@@ -245,6 +245,7 @@ function computerTurnEasy(){
     checkWinner();
 };
 
+
 function onePlayerHard(){
     $('.square').click(function(){
         if (player === playerOne) {
@@ -255,124 +256,153 @@ function onePlayerHard(){
                     $(this).append('<h3>' + playerOne + '</h3>');
                     SQ1 = 1;
                     player = playerTwo;
+                    setTimeout(computerTurnHard, 1000);
                 };
             } else if (square === "SQ2") {
                 if (SQ2 === 0) {
                     $(this).append('<h3>' + playerOne + '</h3>');
                     SQ2 = 1;
                     player = playerTwo;
+                    setTimeout(computerTurnHard, 1000);
                 };
             } else if (square === "SQ3") {
                 if (SQ3 === 0) {
                     $(this).append('<h3>' + playerOne + '</h3>');
                     SQ3 = 1;
                     player = playerTwo;
+                    setTimeout(computerTurnHard, 1000);
                 };
             } else if (square === "SQ4") {
                 if (SQ4 === 0) {
                     $(this).append('<h3>' + playerOne + '</h3>');
                     SQ4 = 1;
                     player = playerTwo;
+                    setTimeout(computerTurnHard, 1000);
                 };
             } else if (square === "SQ5") {
                 if (SQ5 === 0) {
                     $(this).append('<h3>' + playerOne + '</h3>');
                     SQ5 = 1;
                     player = playerTwo;
+                    setTimeout(computerTurnHard, 1000);
                 };
             } else if (square === "SQ6") {
                 if (SQ6 === 0) {
                     $(this).append('<h3>' + playerOne + '</h3>');
                     SQ6 = 1;
                     player = playerTwo;
+                    setTimeout(computerTurnHard, 1000);
                 };
             } else if (square === "SQ7") {
                 if (SQ7 === 0) {
                     $(this).append('<h3>' + playerOne + '</h3>');
                     SQ7 = 1;
                     player = playerTwo;
+                    setTimeout(computerTurnHard, 1000);
                 };
             } else if (square === "SQ8") {
                 if (SQ8 === 0) {
                     $(this).append('<h3>' + playerOne + '</h3>');
                     SQ8 = 1;
                     player = playerTwo;
+                    setTimeout(computerTurnHard, 1000);
                 };
             } else if (square === "SQ9") {
                 if (SQ9 === 0) {
                     $(this).append('<h3>' + playerOne + '</h3>');
                     SQ9 = 1;
                     player = playerTwo;
+                    setTimeout(computerTurnHard, 1000);
                 };
             };
 
             checkWinner();
 
         } else if (player === playerTwo) {
-            var square = $(this).map(function(){ return this.id }).get().join(', ');
-
-
-            console.log(square);
-            if (square === "SQ1") {
-                if (SQ1 === 0) {
-                    $(this).append('<h3>' + playerTwo + '</h3>');
-                    SQ1 = -1;
-                    player = playerOne;
-                };
-            } else if (square === "SQ2") {
-                if (SQ2 === 0) {
-                    $(this).append('<h3>' + playerTwo + '</h3>');
-                    SQ2 = -1;
-                    player = playerOne;
-                };
-            } else if (square === "SQ3") {
-                if (SQ3 === 0) {
-                    $(this).append('<h3>' + playerTwo + '</h3>');
-                    SQ3 = -1;
-                    player = playerOne;
-                };
-            } else if (square === "SQ4") {
-                if (SQ4 === 0) {
-                    $(this).append('<h3>' + playerTwo + '</h3>');
-                    SQ4 = -1;
-                    player = playerOne;
-                };
-            } else if (square === "SQ5") {
-                if (SQ5 === 0) {
-                    $(this).append('<h3>' + playerTwo + '</h3>');
-                    SQ5 = -1;
-                    player = playerOne;
-                };
-            } else if (square === "SQ6") {
-                if (SQ6 === 0) {
-                    $(this).append('<h3>' + playerTwo + '</h3>');
-                    SQ6 = -1;
-                    player = playerOne;
-                };
-            } else if (square === "SQ7") {
-                if (SQ7 === 0) {
-                    $(this).append('<h3>' + playerTwo + '</h3>');
-                    SQ7 = -1;
-                    player = playerOne;
-                };
-            } else if (square === "SQ8") {
-                if (SQ8 === 0) {
-                    $(this).append('<h3>' + playerTwo + '</h3>');
-                    SQ8 = -1;
-                    player = playerOne;
-                };
-            } else if (square === "SQ9") {
-                if (SQ9 === 0) {
-                    $(this).append('<h3>' + playerTwo + '</h3>');
-                    SQ9 = -1;
-                    player = playerOne;
-                };
-            };
-
-            checkWinner();
-
+            computerTurnHard();
         };
     });
+};
+
+function computerTurnHard(){
+    var randomChoice = Math.floor(Math.random() * ((9-1)+1) + 1);
+    var square = "SQ" + randomChoice;
+    var squareID = "#" + square;
+    console.log("Random is: " + square);
+    if (square === "SQ1") {
+        if (SQ1 === 0) {
+            $(squareID).append('<h3>' + playerTwo + '</h3>');
+            SQ1 = -1;
+            player = playerOne;
+        } else {
+            computerTurnHard();
+        };
+    } else if (square === "SQ2") {
+        if (SQ2 === 0) {
+            $(squareID).append('<h3>' + playerTwo + '</h3>');
+            SQ2 = -1;
+            player = playerOne;
+        } else {
+            computerTurnHard();
+        };
+    } else if (square === "SQ3") {
+        if (SQ3 === 0) {
+            $(squareID).append('<h3>' + playerTwo + '</h3>');
+            SQ3 = -1;
+            player = playerOne;
+        } else {
+            computerTurnHard();
+        };
+    } else if (square === "SQ4") {
+        if (SQ4 === 0) {
+            $(squareID).append('<h3>' + playerTwo + '</h3>');
+            SQ4 = -1;
+            player = playerOne;
+        } else {
+            computerTurnHard();
+        };
+    } else if (square === "SQ5") {
+        if (SQ5 === 0) {
+            $(squareID).append('<h3>' + playerTwo + '</h3>');
+            SQ5 = -1;
+            player = playerOne;
+        } else {
+            computerTurnHard();
+        };
+    } else if (square === "SQ6") {
+        if (SQ6 === 0) {
+            $(squareID).append('<h3>' + playerTwo + '</h3>');
+            SQ6 = -1;
+            player = playerOne;
+        } else {
+            computerTurnHard();
+        };
+    } else if (square === "SQ7") {
+        if (SQ7 === 0) {
+            $(squareID).append('<h3>' + playerTwo + '</h3>');
+            SQ7 = -1;
+            player = playerOne;
+        } else {
+            computerTurnHard();
+        };
+    } else if (square === "SQ8") {
+        if (SQ8 === 0) {
+            $(squareID).append('<h3>' + playerTwo + '</h3>');
+            SQ8 = -1;
+            player = playerOne;
+        } else {
+            computerTurnHard();
+        };
+    } else if (square === "SQ9") {
+        if (SQ9 === 0) {
+            $(squareID).append('<h3>' + playerTwo + '</h3>');
+            SQ9 = -1;
+            player = playerOne;
+        } else {
+            computerTurnHard();
+        };
+    };
+    checkWinner();
 };
 
 function twoPlayer(){
