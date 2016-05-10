eaf57c61b6a381ceeb5edc7f2f78d9220ddaa3fc
lukephelan <luke.phelan@hotmail.com>
2016-05-10 11:45:54 +1000

    Split single player into easy and hard mode, both the same atm

--- js/app.js
+++ js/app.js
@@ -80,7 +80,137 @@ startButton.on('click', function(event) {
 });
 
 
-function onePlayer(){
+function onePlayerEasy(){
+    $('.square').click(function(){
+        if (player === playerOne) {
+            var square = $(this).map(function(){ return this.id }).get().join(', ');
+            console.log(square);
+            if (square === "SQ1") {
+                if (SQ1 === 0) {
+                    $(this).append('<h3>' + playerOne + '</h3>');
+                    SQ1 = 1;
+                    player = playerTwo;
+                };
+            } else if (square === "SQ2") {
+                if (SQ2 === 0) {
+                    $(this).append('<h3>' + playerOne + '</h3>');
+                    SQ2 = 1;
+                    player = playerTwo;
+                };
+            } else if (square === "SQ3") {
+                if (SQ3 === 0) {
+                    $(this).append('<h3>' + playerOne + '</h3>');
+                    SQ3 = 1;
+                    player = playerTwo;
+                };
+            } else if (square === "SQ4") {
+                if (SQ4 === 0) {
+                    $(this).append('<h3>' + playerOne + '</h3>');
+                    SQ4 = 1;
+                    player = playerTwo;
+                };
+            } else if (square === "SQ5") {
+                if (SQ5 === 0) {
+                    $(this).append('<h3>' + playerOne + '</h3>');
+                    SQ5 = 1;
+                    player = playerTwo;
+                };
+            } else if (square === "SQ6") {
+                if (SQ6 === 0) {
+                    $(this).append('<h3>' + playerOne + '</h3>');
+                    SQ6 = 1;
+                    player = playerTwo;
+                };
+            } else if (square === "SQ7") {
+                if (SQ7 === 0) {
+                    $(this).append('<h3>' + playerOne + '</h3>');
+                    SQ7 = 1;
+                    player = playerTwo;
+                };
+            } else if (square === "SQ8") {
+                if (SQ8 === 0) {
+                    $(this).append('<h3>' + playerOne + '</h3>');
+                    SQ8 = 1;
+                    player = playerTwo;
+                };
+            } else if (square === "SQ9") {
+                if (SQ9 === 0) {
+                    $(this).append('<h3>' + playerOne + '</h3>');
+                    SQ9 = 1;
+                    player = playerTwo;
+                };
+            };
+
+            checkWinner();
+
+        } else if (player === playerTwo) {
+            var square = $(this).map(function(){ return this.id }).get().join(', ');
+
+
+            console.log(square);
+            if (square === "SQ1") {
+                if (SQ1 === 0) {
+                    $(this).append('<h3>' + playerTwo + '</h3>');
+                    SQ1 = -1;
+                    player = playerOne;
+                };
+            } else if (square === "SQ2") {
+                if (SQ2 === 0) {
+                    $(this).append('<h3>' + playerTwo + '</h3>');
+                    SQ2 = -1;
+                    player = playerOne;
+                };
+            } else if (square === "SQ3") {
+                if (SQ3 === 0) {
+                    $(this).append('<h3>' + playerTwo + '</h3>');
+                    SQ3 = -1;
+                    player = playerOne;
+                };
+            } else if (square === "SQ4") {
+                if (SQ4 === 0) {
+                    $(this).append('<h3>' + playerTwo + '</h3>');
+                    SQ4 = -1;
+                    player = playerOne;
+                };
+            } else if (square === "SQ5") {
+                if (SQ5 === 0) {
+                    $(this).append('<h3>' + playerTwo + '</h3>');
+                    SQ5 = -1;
+                    player = playerOne;
+                };
+            } else if (square === "SQ6") {
+                if (SQ6 === 0) {
+                    $(this).append('<h3>' + playerTwo + '</h3>');
+                    SQ6 = -1;
+                    player = playerOne;
+                };
+            } else if (square === "SQ7") {
+                if (SQ7 === 0) {
+                    $(this).append('<h3>' + playerTwo + '</h3>');
+                    SQ7 = -1;
+                    player = playerOne;
+                };
+            } else if (square === "SQ8") {
+                if (SQ8 === 0) {
+                    $(this).append('<h3>' + playerTwo + '</h3>');
+                    SQ8 = -1;
+                    player = playerOne;
+                };
+            } else if (square === "SQ9") {
+                if (SQ9 === 0) {
+                    $(this).append('<h3>' + playerTwo + '</h3>');
+                    SQ9 = -1;
+                    player = playerOne;
+                };
+            };
+
+            checkWinner();
+
+        };
+    });
+};
+
+function onePlayerHard(){
     $('.square').click(function(){
         if (player === playerOne) {
             var square = $(this).map(function(){ return this.id }).get().join(', ');
